2dd8c69b887b088d4fdba25552fe5f2343a24eba
fsprogamer <fsprogamer@gmail.com>
2017-06-30 16:47:54 +0300

    first

---
+++ Курс2_WPF/Урок 5/HelloWPF/HelloWPF/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Создаем первое окно
+            MainWindow wnd = new MainWindow();
+            if (e.Args.Length == 1)
+                MessageBox.Show("Параметр: \n\n" + e.Args[0]);
+            wnd.Show();
+        }
+    }
+}
