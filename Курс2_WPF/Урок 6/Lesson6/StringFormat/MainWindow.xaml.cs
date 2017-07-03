02e5a5b1be4534e55cbe3756ab340a53abb1f2ce
fsprogamer <fsprogamer@gmail.com>
2017-07-03 16:25:28 +0300

    work on second lesson

---
+++ Курс2_WPF/Урок 6/Lesson6/StringFormat/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace StringFormat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ Курс2_WPF/Урок 6/Lesson6/ValueConverter/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ValueConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+    }
+    public class DebugDummyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Debugger.Break();
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Debugger.Break();
+            return value;
+        }
+    }    
+}
