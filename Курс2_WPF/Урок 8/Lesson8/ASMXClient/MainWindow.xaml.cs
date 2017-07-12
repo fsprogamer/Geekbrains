78720fa0370d2c030b36ad171bcdd46fe84f66bf
fsprogamer <fsprogamer@gmail.com>
2017-07-12 17:49:54 +0300

    Lesson 8

---
+++ Курс2_WPF/Урок 8/Lesson8/ASMXClient/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace ASMXClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceReference.WebServiceSoapClient serviceClient = new ServiceReference.WebServiceSoapClient();
+            string sum = serviceClient.Summ(textBox1.Text, textBox2.Text);
+            textBlock.Text = sum;
+        }
+    }
+}
---
+++ Курс2_WPF/Урок 8/Lesson8/ASMXWebSevice/WebService.asmx.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Services;
+
+namespace ASMXWebSevice
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public string Summ(string number1, string number2)
+        {
+            // Входные параметры объявляем типа String, чтобы принимать от пользователя любые символы,
+            // их анализировать, и при "плохом вводе" сообщать по-русски.
+            float a, b;
+            bool flag = float.TryParse(number1, System.Globalization.NumberStyles.Number,
+            System.Globalization.NumberFormatInfo.CurrentInfo, out a);
+            if (flag == false) return "В первом поле должно быть число";
+            flag = float.TryParse(number2, System.Globalization.NumberStyles.Number,
+            System.Globalization.NumberFormatInfo.CurrentInfo, out b);
+            if (flag == false) return "В первом поле должно быть число";
+            return "Сумма:" + (a + b).ToString();
+        }
+    }
+}
