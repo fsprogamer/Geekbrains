6ec1c9bca7ccc3243f5fec84f446ad8eb563a1f2
fsprogamer <fsprogamer@gmail.com>
2017-07-16 18:44:49 +0300

    Lesson 9 end

---
+++ Курс2_WPF/Урок 8/Lesson8/WebAPIClient/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WebAPIClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+            client.BaseAddress = new Uri("http://localhost:6832/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private async void allproductButton_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<Product> products = await GetProductsAsync(client.BaseAddress + "api/Products");
+            productDataGrid.ItemsSource = products;
+        }
+
+        private async void idproductButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<Product> products = new List<Product>();
+            if (idproductTextBox.Text != String.Empty)
+            {
+                Product product = await GetProductAsync(client.BaseAddress + "api/Products/" + idproductTextBox.Text);
+                if(product != null)
+                    products.Add(product);
+            }
+            else
+            {
+                products = (List<Product>)await GetProductsAsync(client.BaseAddress + "api/Products");
+            }
+            productDataGrid.ItemsSource = products;
+        }
+       
+        static async Task<IEnumerable<Product>> GetProductsAsync(string path)
+        {
+            IEnumerable<Product> products = null;
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    products = await response.Content.ReadAsAsync<IEnumerable<Product>>();
+                }
+            }
+            catch(Exception) {
+            }
+            return products;
+        }
+        static async Task<Product> GetProductAsync(string path)
+        {
+            Product product = null;
+            try {                 
+                HttpResponseMessage response = await client.GetAsync(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsAsync<Product>();
+                }
+            }
+            catch(Exception) {
+            }
+            return product;
+        }
+    }
+}
---
+++ Курс2_WPF/Урок 8/Lesson8/WebAPIService/Controllers/ProductsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebAPIService.Models;
+
+namespace WebAPIService.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+        new Product { Id = 1, Name = "Чай Ахмат", Category = "Бакалея", Price = 100 },
+        new Product { Id = 2, Name = "Кукла Барби", Category = "Игрушки", Price = 1000 },
+        new Product { Id = 3, Name = "Дрель Интерскол", Category = "Инструменты", Price = 3000 }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        //[HttpPost]        
+        //public IHttpActionResult Post(Product product)
+        //{
+        //    if (product == null)
+        //    {
+        //        return BadRequest("Product cannot be null");
+        //    }
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    product.Id = products.Count();
+        //    products[product.Id] = product;
+        //    return CreatedAtRoute("GetById", new { id = product.Id }, product);
+        //}
+
+        //[HttpPut]        
+        //public IHttpActionResult Put(string id, Product product)
+        //{
+        //    int _id = Convert.ToInt32(id);
+        //    if (product == null)
+        //    {
+        //        return BadRequest("Product cannot be null");
+        //    }
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+        //    if (product.Id != _id)
+        //    {
+        //        return BadRequest("product.id does not match id parameter");
+        //    }
+
+        //    if (!products.Select ..Contains(id))
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _products[id] = product;
+        //    return new StatusCodeResult(HttpStatusCode.NoContent, this);
+        //}
+
+        //[HttpDelete]
+        //[Route("{id}")]
+        //public IHttpActionResult Delete(string id)
+        //{
+        //    Product product = null;
+        //    products.TryRemove(id, out product);
+        //    return new StatusCodeResult(HttpStatusCode.NoContent, this);
+        //}
+    }
+}
+
